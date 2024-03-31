880b5f2f626032b5cf9a2b5f2eb2e99a8fbe7eb3
Shrey Raj <shreyraj1653@gmail.com>
2024-03-31 22:27:07 +0530

    bugs

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,16 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
-    '/api/webhook/clerk',
+    '/api/webhooks/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
   ],
   ignoredRoutes: [
-    '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing'
   ]
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
